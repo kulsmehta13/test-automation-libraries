844dd29de56ed0d20a2e1330e408bb8b5830f222
Kuldip Mehta <50161556+kulsmehta13@users.noreply.github.com>
2024-02-19 15:59:56 -0500

    Tal 2 (#4)
    * added setup.py
    
    * update yml
    
    * ss
    
    * removed old workflow
    
    * added toml
    
    * updated
    
    * aa

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='test_automation_libraries',
+    name='test-automation-libraries',
     version='0.0.1',
     packages=find_packages(),
     install_requires=[
