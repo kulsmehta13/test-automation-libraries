398a001697b7dccdc1fa73c6506db9b1c03ab2ec
Kuldip Mehta <50161556+kulsmehta13@users.noreply.github.com>
2024-02-19 16:03:32 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='test-automation-libraries',
+    name='test_automation_libraries',
     version='0.0.1',
     packages=find_packages(),
     install_requires=[
